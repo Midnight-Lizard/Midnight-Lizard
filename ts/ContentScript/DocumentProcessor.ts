0fde94e1f543240ac2881ef920753a25ddef0b61
Pavel Agarkov <pavel.agarkov@gmail.com>
2017-10-31 10:25:01 +0300

    converting irrational to fixed

--- ts/ContentScript/DocumentProcessor.ts
+++ ts/ContentScript/DocumentProcessor.ts
@@ -115,7 +115,7 @@ namespace MidnightLizard.ContentScript
             const
                 invertedBackgroundImageFilter = [
                     this.shift.BackgroundImage.saturationLimit < 1 ? `saturate(${this.shift.BackgroundImage.saturationLimit})` : "",
-                    `brightness(${1 - this.shift.Background.lightnessLimit}) hue-rotate(180deg) invert(1)`,
+                    `brightness(${(1 - this.shift.Background.lightnessLimit).toFixed(2)}) hue-rotate(180deg) invert(1)`,
                     this._settingsManager.currentSettings.blueFilter !== 0 ? `url("#ml-blue-filter")` : ""
                 ].filter(f => f).join(" ").trim(),
                 backgroundImageFilter = [
@@ -1185,7 +1185,7 @@ namespace MidnightLizard.ContentScript
                                         tag.computedStyle!.filter != this._css.none ? tag.computedStyle!.filter : "",
                                         imgSet.saturationLimit < 1 ? `saturate(${imgSet.saturationLimit})` : "",
                                         imgSet.lightnessLimit < 1 && !doInvert ? `brightness(${imgSet.lightnessLimit})` : "",
-                                        doInvert ? `brightness(${1 - this.shift.Background.lightnessLimit})` : "",
+                                        doInvert ? `brightness(${(1 - this.shift.Background.lightnessLimit).toFixed(2)})` : "",
                                         doInvert ? "hue-rotate(180deg) invert(1)" : "",
                                         this._settingsManager.currentSettings.blueFilter !== 0 ? `url("#ml-blue-filter")` : ""
                                     ].filter(f => f).join(" ").trim()
@@ -1237,7 +1237,7 @@ namespace MidnightLizard.ContentScript
                                 bgFilter = [
                                     bgImgSet.saturationLimit < 1 ? `saturate(${bgImgSet.saturationLimit})` : "",
                                     bgImgLight < 1 ? `brightness(${bgImgLight})` : "",
-                                    doInvert ? `brightness(${1 - this.shift.Background.lightnessLimit})` : "",
+                                    doInvert ? `brightness(${(1 - this.shift.Background.lightnessLimit).toFixed(2)})` : "",
                                     doInvert ? "hue-rotate(180deg) invert(1)" : "",
                                     this._settingsManager.currentSettings.blueFilter !== 0 ? `url("#ml-blue-filter")` : ""
                                 ].filter(f => f).join(" ").trim();
@@ -1338,7 +1338,7 @@ namespace MidnightLizard.ContentScript
                             filterValue = [
                                 tag.computedStyle!.filter != this._css.none ? tag.computedStyle!.filter! : "",
                                 bgrSet.saturationLimit < 1 ? `saturate(${bgrSet.saturationLimit})` : "",
-                                `brightness(${1 - bgrSet.lightnessLimit})`,
+                                `brightness(${(1 - bgrSet.lightnessLimit).toFixed(2)})`,
                                 `hue-rotate(180deg) invert(1)`,
                                 this._settingsManager.currentSettings.blueFilter !== 0 ? `url("#ml-blue-filter")` : "",
                                 `brightness(${txtSet.lightnessLimit})`
