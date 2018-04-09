f9bb03531d794b20f3049930b8f97102f99f143c
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-04-09 22:02:47 +0300

    bing-maps processing

---
+++ ts/Custom/bing-maps.ts
@@ -0,0 +1,46 @@
+namespace MidnightLizard.Custom
+{
+    let mapTypeSelect: HTMLElement;
+    const classObserver = new MutationObserver(classObserverCallback),
+        classObserverConfig: MutationObserverInit = { attributes: true, attributeFilter: ["class"] };
+
+    const interval = setInterval(() =>
+    {
+        mapTypeSelect = document.getElementById("MicrosoftNav")!;
+        if (mapTypeSelect)
+        {
+            clearInterval(interval);
+            classObserver.observe(mapTypeSelect, classObserverConfig);
+            if (mapTypeSelect.classList.contains("Dark"))
+            {
+                classObserverCallback(null as any, null as any);
+            }
+        }
+    }, 100);
+
+    function classObserverCallback(mutations: MutationRecord[], observer: MutationObserver)
+    {
+        const canvasArr = document.querySelectorAll("#tileCanvasId,#labelCanvasId") as any as HTMLCanvasElement[];
+        const noInvert = mapTypeSelect.classList.contains("Dark");
+        if (canvasArr && canvasArr.length > 0)
+        {
+            const parent = canvasArr[0].parentElement!;
+            if (noInvert)
+            {
+                parent.style.setProperty("--ml-no-invert", noInvert.toString());
+                parent.style.setProperty("--ml-background-background-color", "Text");
+            }
+            else
+            {
+                parent.style.removeProperty("--ml-background-background-color");
+                parent.style.removeProperty("--ml-no-invert");
+            }
+
+            for (const canvas of canvasArr)
+            {
+                canvas.classList.toggle("ml-fix");
+                canvas.setAttribute("ml-timestamp", Date.now().toString(16));
+            }
+        }
+    }
+}
