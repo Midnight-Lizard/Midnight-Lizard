1bf8135c47956b3b142c58c50b09ded06eeb0bcb
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-10-14 20:41:53 +0300

    sets comparison

--- ts/Utils/Array.ts
+++ ts/Utils/Array.ts
@@ -11,4 +11,13 @@ namespace MidnightLizard.Util
             return ar;
         }, new Array<TSource[]>());
     }
+
+    export function setsAreEqual<TSource>(set1: Set<TSource>, set2: Set<TSource>)
+    {
+        if (!set1 && !!set2 || !!set1 && !set2) return false;
+        if (!set1 && !set2) return true;
+        if (set1.size !== set2.size) return false;
+        for (const a of set1) if (!set2.has(a)) return false;
+        return true;
+    }
 }
