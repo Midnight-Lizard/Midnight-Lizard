42dfe324db67a0b96b29850788977937e75dcb10
Pavel Agarkov <pavel.agarkov@gmail.com>
2019-07-10 22:12:04 +0300

    adding docker image tag

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,5 @@
 # eval $(docker-machine env default --shell bash)
 mkdir releases
-docker build -t midnight-lizard .
-docker run --rm -it -v $(pwd -$1)/releases:/build/releases midnight-lizard
+TAG=midnight-lizard:$(date +"%Y-%m-%d--%H-%M-%S")
+docker build -t $TAG .
+docker run --rm -it -v $(pwd -$1)/releases:/build/releases $TAG
